ba91db788cf17d3470588a01823f60e08c1b38da
marc10601060 <74018760+marc10601060@users.noreply.github.com>
2020-11-13 23:14:31 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 STALKER="/var/www/stalker_portal"
 
 if [ ! -d $STALKER/deploy ]; then
-        wget -O /tmp/stalker.zip http://live.dedyn.io:2015/ministra-5.6.1.zip
-        unzip /tmp/stalker.zip -d /var/www/
+        wget -O /tmp/stalker.zip https://archive.org/download/ministra-5.6.1/ministra-5.6.1.zip        
+        unzip /tmp/ministra-5.6.1.zip -d /var/www/
         mv /var/www/stalker_portal-*/* /var/www/stalker_portal/
         rm -rf /var/www/stalker_portal-*
 fi
 
 if [ ! -s $STALKER/server/custom.ini ]; then
-        wget -O $STALKER/server/custom.ini https://raw.githubusercontent.com/sybdata/Ministra/master/docker/stalker_custom.ini
+        wget -O $STALKER/server/custom.ini https://raw.githubusercontent.com/marc10601060/Ministra/master/docker/stalker_custom.ini
 fi
 
 sed -i '/\bmysql_tzinfo_to_sql\b/g' $STALKER/deploy/build.xml
